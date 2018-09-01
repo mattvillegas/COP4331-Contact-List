31b39021826a538dad873343317b38b881694d6a
joshualecas <jlecas@knights.ucf.edu>
2018-09-01 16:09:00 -0400

    Added home, register, and dash components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { DashComponent } from './dash/dash.component';
+
+const appRoutes: Routes =[
+  { path:'', component: HomeComponent },
+  { path: 'register', component: RegisterComponent},
+  { path:'user/dash', component: DashComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule
